f7f0a1cec6dba59e6cec238b5595f85366e1d270
Krasva687 <krasva687@gmail.com>
2021-10-17 12:44:00 +0300

    java_config added

--- spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -3,21 +3,22 @@ package ru.sber.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 import ru.sber.services.FirstService
 
 @Configuration
-@ComponentScan
+@ComponentScan("ru.sber.services")
 class ServicesConfig {
-    @Bean
+
     fun service(): FirstService {
         return FirstService()
     }
 
-    @Bean
+
     fun secondService() {
     }
 }
 
 @Configuration
-@ComponentScan("ru.sber.anotherservices")
+@ComponentScan("ru.sber.services")
 class AnotherServicesConfig
--- spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
@@ -1,28 +1,33 @@
 package ru.sber.services
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
+@Configuration
+@Component("firstService")
 class FirstService {
     override fun toString(): String {
         return "I am firstService"
     }
 }
-
+@Configuration
+@Component("secondService")
 class SecondService {
     override fun toString(): String {
         return "I am secondService"
     }
 }
-
-@Component
+@Configuration
+@Component("thirdService")
 class ThirdService {
     override fun toString(): String {
         return "I am thirdService"
     }
 }
-
-@Service
+@Configuration
+@Component("fourthService")
 class FourthService {
     override fun toString(): String {
         return "I am fourthService"
